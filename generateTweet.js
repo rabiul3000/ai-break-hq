a489ff8784bb1f17b242c5d91ea35e3dd2f8e9e8
Md Rabiul <mdrabiul.asia@gmail.com>
2025-07-05 00:27:54 +0600

    Update generateTweet.js

--- generateTweet.js
+++ generateTweet.js
@@ -9,8 +9,9 @@ const openai = new OpenAI({
 async function generateTweetFromTitle(title, link) {
   try {
     const prompt = `
-      Generate a creative, engaging tweet (200 characters or less) about based on this Title: ${title} and this Link: ${link}.
+      Generate a creative, engaging tweet (200 characters or less) about based on this Title: ${title}.
       Use a witty tone, include a relevant hashtag, and keep it concise.
+   - Add this full Link: ${link} to read the full article without any anchor brackets.
     `;
     console.log({ msg: "generating tweet...", title, link });
     const completion = await openai.chat.completions.create({
